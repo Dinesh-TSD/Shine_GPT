6f572af870109b1255aae1a7d83b66fdff277c7e
Dinesh-TSD <dineshtsd76@gmail.com>
2025-07-25 00:19:21 +0530

    controllers added

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -3,6 +3,6 @@ import { getAllUsers } from "../controllers/userController.js";
 
 const router = Router()
 
-router.get("/allusers",getAllUsers)
+router.get("/",getAllUsers)
 
 export default router;
